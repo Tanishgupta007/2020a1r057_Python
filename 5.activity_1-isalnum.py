f62adcb0ea7a538689608a1bf5bb3be3fc6cfb81
Tanishgupta007 <tanishm222@gmail.com>
2022-09-08 22:44:39 +0530

    Isalnum

---
+++ 5.activity_1-isalnum.py
@@ -0,0 +1,12 @@
+# string
+name1 = "Tanish"
+print(name1.isalnum())
+
+# string with whitespace
+name1 = " Tanish "
+print(name1.isalnum())
+
+
+# contains non alpha numeric characters
+name1 = "@Tanish!"
+print(name1.isalnum())
