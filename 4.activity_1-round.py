8c9c24f4d2965c967e1203099b8db08612a4877a
Tanishgupta007 <tanishm222@gmail.com>
2022-09-08 22:36:01 +0530

    Round Function

---
+++ 4.activity_1-round.py
@@ -0,0 +1,14 @@
+# round function
+
+# integer
+print(round(5))
+
+# float
+print(round(5.3))
+print(round(5.6))
+print(round(5.8))
+
+
+print(round(2.676 , 2))
+print(round(5.676 , 2))
+print(round(5.856 , 2))
