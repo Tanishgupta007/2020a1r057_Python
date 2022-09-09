5a8eec7711e5a30eb136a1f67daa309e9b732cf7
Tanishgupta007 <tanishm222@gmail.com>
2022-09-09 20:00:28 +0530

    Smallest Item and string in the List

--- 3.activity_1-min.py
+++ 3.activity_1-min.py
@@ -5,3 +5,8 @@ number = [3,4,5,2,65,32,22]
 min_number = min(number)
 
 print("The Smallest Number in the list ",number," is " ,min_number)
+
+# the smallest string in a list
+string = ['Tanish' , 'gupta', 'pywsdth']
+min_str = min(string , key=len)
+print("The Smallest string in the list ",string," is " ,min_str)
