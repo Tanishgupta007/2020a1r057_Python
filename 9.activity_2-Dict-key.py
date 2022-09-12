83ed8d0d6b23d8e8f2cc43cce1e62495a223c28f
Tanishgupta007 <tanishm222@gmail.com>
2022-09-12 11:00:18 +0530

    # Access Keys using build .keys()

---
+++ 9.activity_2-Dict-key.py
@@ -0,0 +1,17 @@
+# Access Keys using build .keys()
+
+statesAndCapitals = {
+    "Gujarat" : "Gandhinagar",
+    "Maharashtra" : "Mumbai",
+    "Rajasthan" : "Jaipur",
+    "Bihar": "Patna"
+}
+
+keys = statesAndCapitals.keys()
+print(keys)
+
+
+
+  
+
+
