a3deedc499d806b3c1eaf154a5a186fbed061139
Tanishgupta007 <tanishm222@gmail.com>
2022-09-12 11:07:47 +0530

    # Access Keys using without using a key()

---
+++ 10.activity_2-Dict-key2.py
@@ -0,0 +1,15 @@
+# Access Keys using without using a key()
+
+
+statesAndCapitals = {
+    "Gujarat" : "Gandhinagar",
+    "Maharashtra" : "Mumbai",
+    "Rajasthan" : "Jaipur",
+    "Bihar": "Patna"
+}
+
+print("List of given states\n")
+
+for state in statesAndCapitals:
+    print(state)
+    
